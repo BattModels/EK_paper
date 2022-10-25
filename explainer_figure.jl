0cf4697545e0bc4e96ff4be48ff74da61f531c38
Rachel Kurchin <rkurchin@cmu.edu>
2022-10-25 18:04:09 -0400

    update environment, explainer figure (hopefully) done
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- explainer_figure.jl
+++ explainer_figure.jl
@@ -12,23 +12,23 @@ tls = 20
 # model/data options
 x = 0:0.002:0.999
 I₀ = 900
-λ = 0.224
+λ = 0.3
 model = Marcus(I₀, λ) # inverts at a current of I₀ and voltage of λ
 T = 375
 Ω = 0.1
-I_force = 0.04I₀
-pbs₀ = find_phase_boundaries(I_force, model, muoA=0, muoB=0, Ω=Ω, T=T, warn=false)
+I_force = I₀/40
+pbs₀ = find_phase_boundaries(I_force, model, Ω=Ω, T=T, warn=false, lin_thresh=2)
 x₀ = 0.84
 x0_ind = searchsorted(x, x₀).start
 I_calc = I_force/(1-x₀)
 
 # compute things
-µth_vals = µ_thermo(x, muoA=0, muoB=0, Ω=Ω, T=T)
-µ = µ_kinetic(I_force, model, muoA=0, muoB=0, Ω=Ω, T=T)
+µth_vals = µ_thermo(x, Ω=Ω, T=T)
+µ = µ_kinetic(I_force, model, Ω=Ω, T=T)
 µ_vals = µ.(x)
 Δµ = µ_vals[x0_ind]-µth_vals[x0_ind]
-g = g_kinetic(I_force, model, muoA=0, muoB=0, Ω=Ω, T=T)
-Δg = g(x₀) - g_thermo(x₀, muoA=0, muoB=0, Ω=Ω, T=T)
+g = g_kinetic(I_force, model, Ω=Ω, T=T)
+Δg = g(x₀) - g_thermo(x₀, Ω=Ω, T=T)
 
 # make figure
 f = Figure(resolution=(1250,900))
@@ -50,14 +50,14 @@ grid = f[1:6,1] = GridLayout()
 band!(µ_ax, x[1:x0_ind], µth_vals[1:x0_ind], µ_vals[1:x0_ind], color=(:green, 0.3))
 
 # µ values (plotted second so they're on top of shading)
-lines!(µ_ax, x, µ_vals, label=L"\mu_{\mathrm{kin}}(x; I=I_0/25)", color=:orange)
+lines!(µ_ax, x, µ_vals, label=L"\mu_{\mathrm{kin}}(x; I=I_0/40)", color=:orange)
 lines!(µ_ax, x, µth_vals, label=L"\mu_{\mathrm{thermo}}(x)", linestyle=:dot, color=:steelblue3)
 
 # tweaks/annotations
-limits!(µ_ax, (0,1), (-0.4Ω, 2.2Ω))
+limits!(µ_ax, (0,1), (-0.4Ω, 2.3Ω))
 axislegend(µ_ax, position=:rt)
 arrows!(µ_ax, [x₀, x₀], [µth_vals[x0_ind], µ_vals[x0_ind]-1e-3], [0, 0], [Δµ-2e-3, -Δµ+2e-3], color=:mediumpurple4, linewidth=3, arrowsize=10)
-text!(µ_ax, x₀+0.01, 0.01, text=L"\Delta \mu(x_0)", color=:mediumpurple4)
+text!(µ_ax, x₀+0.01, Ω/4, text=L"\Delta \mu(x_0)", color=:mediumpurple4)
 
 # add Tafel plot inset
 inset_ax = Axis(grid[1:3,1],
@@ -68,7 +68,7 @@ inset_ax = Axis(grid[1:3,1],
     backgroundcolor=:gray95,
     yscale=log10,
     xticklabelsize=tls-2,
-    yticks=([I_calc], [L"\frac{I_0/25}{1-x_0}"]),
+    yticks=([I_calc], [L"\frac{I_0/40}{1-x_0}"]),
     yticklabelsize=tls
 )
 
@@ -88,7 +88,7 @@ arrows!(inset_ax, [0, Δµ], [I_calc, I_calc], [Δµ-1e-3, -Δµ+1e-3], [0,0], c
 text!(inset_ax, 0.005, 1.04*I_calc, text=L"\Delta \mu(x_0)", color=:mediumpurple4)
 
 textx = 0.33
-texty = -0.02
+texty = -0.01
 text!(µ_ax, textx, texty, text=L"\Delta g(x_0)", color=:green)
 arrows!(µ_ax, [textx+0.13], [texty+0.01], [0.13], [0.012], color=:green4, linewidth=2)
 
@@ -96,27 +96,27 @@ arrows!(µ_ax, [textx+0.13], [texty+0.01], [0.13], [0.012], color=:green4, linew
 g_ax = Axis(grid[4:6,1], 
     xticks = ([0,0.5,x₀,1.0],["0","0.5","x₀","1.0"]),
     xticklabelsize = tls,
-    yticks = ([0, 0.1*Ω, 0.2*Ω], ["0", "Ω/10", "Ω/5"]),
+    yticks = ([0, Ω/5, 2Ω/5], ["0", "Ω/5", "2Ω/5"]),
     ylabel = "molar Gibbs free energy",
     yticklabelsize = tls,
     xgridvisible = false,
     ygridvisible = false
     )
 
-limits!(g_ax, (0,1), (-0.04Ω, 0.2Ω))
+limits!(g_ax, (0,1), (0.17Ω, 0.47Ω))
 
-lines!(g_ax, x[1:end-1], g.(x[1:end-1]), label=L"g_{\mathrm{kin}}(x,I=I_0/25)", color=:orange)
-lines!(g_ax, x, g_thermo(x, muoA=0, muoB=0, Ω=Ω, T=T), label=L"g_{\mathrm{thermo}}(x)", linestyle=:dot, color=:steelblue3)
+lines!(g_ax, x[1:end-1], g.(x[1:end-1]), label=L"g_{\mathrm{kin}}(x;I=I_0/40)", color=:orange)
+lines!(g_ax, x, g_thermo(x, Ω=Ω, T=T), label=L"g_{\mathrm{thermo}}(x)", linestyle=:dot, color=:steelblue3)
 
 # again, some tweaks and annotations
 axislegend(g_ax, position=:lt)
 
-arrows!(g_ax, [x₀, x₀], [g_thermo(x₀, muoA=0, muoB=0, Ω=Ω, T=T), g(x₀)], [0,0], [Δg-1e-4, -Δg+1e-4], color=:green, linewidth=3, arrowsize=10)
-text!(g_ax, x₀+0.015, 0.06*Ω, text=L"\Delta g(x_0)", color=:green)
+arrows!(g_ax, [x₀, x₀], [g_thermo(x₀, Ω=Ω, T=T), g(x₀)], [0,0], [Δg-1e-4, -Δg+1e-4], color=:green, linewidth=3, arrowsize=10)
+text!(g_ax, x₀+0.015, 0.33Ω, text=L"\Delta g(x_0)", color=:green)
 
 
 lines!(g_ax, pbs₀, g(pbs₀), linestyle=:dash, color=:gray25, linewidth=2)
-text!(g_ax, 0.44, 0.0033, text="common tangent", rotation=0.42, color=:gray25, textsize=20)
+text!(g_ax, 0.44, 0.273Ω, text="common tangent", rotation=0.53, color=:gray25, textsize=20)
 
 # now the phase map
 pb_grid = f[1:6,2] = GridLayout()
@@ -131,14 +131,14 @@ pb_ax_1 = Axis(pb_grid[3:5,1],
     ylabel = "Current"
     )
 
-limits!(pb_ax_1, 0,1, 0, 3.5*I_force)
+limits!(pb_ax_1, 0,1, 0, I₀/10)
 
 # this is the code that constructed the phase diagram and saved the data. I precomputed and saved it so iterating on the figure design would be faster, but it should all run just fine
-# pbs_top, I_top = phase_diagram(model; I_step=0.0005I₀, I_max=0.14I₀, Ω=Ω, muoA=0, muoB=0, warn=false, T=T)
-# pbs_bottom, I_bottom = phase_diagram(model; intercalate=false, I_start=0.0001I₀, I_step=0.0001I₀, I_max=0.0036I₀, Ω=Ω, muoA=0, muoB=0, warn=false, T=T)
+# pbs_top, I_top = phase_diagram(model; I_step=0.0005I₀, I_max=0.08I₀, Ω=Ω, warn=false, T=T, lin_thresh=2)
+# pbs_bottom, I_bottom = phase_diagram(model; I_start=-0.002I₀, I_step=0.0001I₀, I_max=-0.0001I₀, Ω=Ω, warn=false, T=T, start_guess=[0.058, 0.978], lin_thresh=2)
 
-# pbs = vcat(pbs_bottom[Int(length(pbs_bottom)/2):-1:1], pbs_top, pbs_bottom[end:-1:Int(length(pbs_bottom)/2)+1])
-# I = vcat(-I_bottom[Int(length(I_bottom)/2):-1:1], I_top, -I_bottom[end:-1:Int(length(I_bottom)/2)+1])
+# pbs = vcat(pbs_bottom[1:Int(length(pbs_bottom)/2)], pbs_top, pbs_bottom[Int(length(pbs_bottom)/2)+1:end])
+# I = vcat(I_bottom[1:Int(length(I_bottom)/2)], I_top, I_bottom[Int(length(I_bottom)/2)+1:end])
 
 # open("data/pbs_I.txt", "w") do io
 #     writedlm(io, [pbs I])
@@ -163,7 +163,7 @@ pb_ax_2 = Axis(pb_grid[6,1],
     ygridvisible = false,
     )
 
-limits!(pb_ax_2, 0,1, -3.24, 0)
+limits!(pb_ax_2, 0,1, -0.002I₀, 0)
 lines!(pb_ax_2, pbs, I, color=:darkgray)
 band!(pb_ax_2, pbs, zeros(length(pbs)).-I_force, I, color=(:darkgrey, 0.3))
 
