3e01f8b70e0c9825888592ed8011bce828541f4d
Rachel Kurchin <rkurchin@cmu.edu>
2022-12-05 18:56:48 -0500

    3D figure for SI now includes other models
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- sifig_3dpd.jl
+++ sifig_3dpd.jl
@@ -6,54 +6,71 @@ using DataFrames
 using Interpolations
 
 bv = ButlerVolmer(300, 0.5)
-T_vals = 250:10:590
+m = Marcus(2700, λ) # max current ~900
+amhc = AsymptoticMarcusHushChidsey(45000, λ)
+models = [bv, m, amhc]
+model_colors = Dict(bv=>:teal, m=>:mediumorchid4, amhc=>:goldenrod)
 
-# x = Float32[]
-# y = Float32[]
-# z = Float32[]
+T_vals = 270:10:590
 
-# for T in T_vals
-#     I_step = round(300000 * T^(-2), digits=1)
-#     println(T, " ", I_step)
-#     pbs, I = phase_diagram(bv, muoA=0, muoB=0, I_max=1000, I_step=I_step, T=T, start_guess=[0.01, 0.99], tol=1e-2)
-#     T_vec = T .* ones(length(I))
-#     append!(x, pbs)
-#     append!(y, T_vec)
-#     append!(z, I)
+# for m in models
+#     println(nameof(typeof(m)))
+#     x = Float32[]
+#     y = Float32[]
+#     z = Float32[]
+
+#     # NB that this data required a bit of manual cleaning afterwards to make sure it was monotonic for the interpolants below, the results of this cleaning are saved in the repo
+#     for T in T_vals
+#         I_step = round(300000 * T^(-2), digits=1)
+#         println(T, " ", I_step)
+#         pbs, I = phase_diagram(m, muoA=0, muoB=0, I_max=1000, I_step=I_step, T=T, start_guess=[0.01, 0.99], tol=1e-2)
+#         T_vec = T .* ones(length(I))
+#         append!(x, pbs)
+#         append!(y, T_vec)
+#         append!(z, I)
+#     end
+
+#     open("./data/sifig/$(nameof(typeof(m))).txt","w") do io
+#         writedlm(io, [x y z])
+#     end
 # end
 
-# open("./data/bv3d.txt","w") do io
-#     writedlm(io, [x y z])
-# end
+f = Figure(resolution=(1500, 400), figure_padding= (10,50,15,15))
+ax_args = Dict(:azimuth=>3pi/4, :elevation=>pi/12, :aspect=>(10,8,6), :ylabel=>"T [K]", :zlabel=>"I [arb]")
+ax1 = Axis3(f[1,1]; title="Butler-Volmer", ylabel=L"\textrm{I }[I_0]", ax_args...)
+ax2 = Axis3(f[1,2]; title="Marcus", ax_args...)
+ax3 = Axis3(f[1,3]; title="asymptotic MHC", ax_args...)
+axes = [ax1, ax2, ax3]
+zlims!.(axes, Ref((0,1000)))
 
-data = readdlm("./data/bv3d.txt")
-df = DataFrame(data, [:x, :T, :I])
+for (m, ax) in zip(models, axes)
+    println(nameof(typeof(m)))
+    data = readdlm("./data/sifig/$(nameof(typeof(m)))_clean.txt")
+    df = DataFrame(data, [:x, :T, :I])
 
-# to get a draggable scatterplot with GLMakie...
-# f = Figure()
-# a = Axis3(f[1,1], ylabel="T", zlabel="I")
-# limits!(a, (0,1), (250, 600), (0, 1000))
-# scatter!(a, df.x, df.T, df.I)
-# f
+    # to get a draggable scatterplot with GLMakie...
+    # f = Figure()
+    # a = Axis3(f[1,1], ylabel="T", zlabel="I")
+    # limits!(a, (0,1), (250, 600), (0, 1000))
+    # scatter!(a, df.x, df.T, df.I)
+    # f
 
-# to get `surface` to work, construct 2D interpolants to individual maps and then build a regular grid in x, T and call interpolant to get I
-# this also required a bit of manual cleanup of the bv3d.txt file to get rid of nonmonotonicities, but not too hard to find with the `issorted` function...
-regular_x = 0.0:0.03:0.99
-regular_T = T_vals
-grid_I = Matrix{Union{Float64,Missing}}(undef, 34, 35)
+    # construct 2D interpolants to individual maps and then build a regular grid in x, T and call interpolant to get I
+    regular_x = 0.0:0.03:0.99
+    regular_T = T_vals
+    grid_I = Matrix{Union{Float64,Missing}}(undef, length(regular_x), length(regular_T))
 
-for i in 1:35
-    T = regular_T[i]
-    subset = df[df.T .== T, :]
-    # interpolate on an irregular grid, give zero outside provided domain
-    interp = extrapolate(interpolate((subset.x,), subset.I, Gridded(Linear())), 0)
-    grid_I[:, i] .= interp(regular_x)
+    for i in 1:length(T_vals)
+        T = regular_T[i]
+        # println(T)
+        subset = df[df.T .== T, :]
+        # interpolate on an irregular grid, give zero outside provided domain
+        interp = extrapolate(interpolate((subset.x,), subset.I, Gridded(Linear())), 0)
+        grid_I[:, i] .= interp(regular_x)
+    end
+
+    wireframe!(ax, regular_x, regular_T, grid_I, linewidth=2, color=model_colors[m])
 end
-
-f = Figure(figure_padding= (10,40,10,10))
-ax = Axis3(f[1,1], azimuth=3pi/4, elevation=pi/12, aspect=(10, 8, 5), ylabel="T [K]", zlabel="I [arb]")
-wireframe!(regular_x, regular_T, grid_I, linewidth=2)
-zlims!(ax, (0,1000))
 
 save("3d.png", f)
 f
